0116c4593fc6e5b363dd9ac706f7cb157ce48097
Toshinori Tsugita <tsugita.dev@gmail.com>
2020-03-24 20:02:05 +0900

    Init

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import { ConfigurationFactory } from 'webpack'
+import path from 'path'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
+const config: ConfigurationFactory = () => {
+  return {
+    entry: {
+      'content-scripts': path.join(__dirname, 'src/content-scripts.ts'),
+      'background': path.join(__dirname, 'src/background.ts')
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
+          use: 'ts-loader',
+          exclude: '/node_modules/'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['ts', 'js']
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'public', to: '.' }
+      ])
+    ]
+  }
+}
+
+export default config
